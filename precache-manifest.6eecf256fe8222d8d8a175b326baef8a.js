2057e3920cebfcdd27abcc7e7aabe04979e66eb1
h7x4 <h7x4abk3g@protonmail.com>
2020-09-10 18:01:03 +0200

    Updates

---
+++ precache-manifest.6eecf256fe8222d8d8a175b326baef8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2cebc564cb03cbec96cf32af067770",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "835aeda73de754c4831a",
+    "url": "/react-tutorial/static/css/main.270a08d5.chunk.css"
+  },
+  {
+    "revision": "ea3cde09963370a14ef0",
+    "url": "/react-tutorial/static/js/2.8e66d86f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.8e66d86f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "835aeda73de754c4831a",
+    "url": "/react-tutorial/static/js/main.e075db4d.chunk.js"
+  },
+  {
+    "revision": "d7697cf6d32f37c92610",
+    "url": "/react-tutorial/static/js/runtime-main.b4501e2a.js"
+  }
+]);
